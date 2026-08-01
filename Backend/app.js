997c0b197648ba73f67304b378fecd9b47469f43
sreyassubu <sreyas6999@gmail.com>
2026-08-01 22:40:38 +0530

    Prepared backend for render deployment

--- Backend/app.js
+++ Backend/app.js
@@ -34,6 +34,7 @@ async function main() {
 //Routes
 app.use("/users",userRouter);
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
